99dc881079b17063370d76a60a9b73cc216a2ffa
Panzerotto <lorenzoduchi94@gmail.com>
2026-05-20 19:15:19 +0100

    Update __init__.py

--- soil_course/__init__.py
+++ soil_course/__init__.py
@@ -2,4 +2,5 @@ from .texture_structure import *
 from .energy_soil_water import *
 from .grains_to_pores import *
 from .darcy_experiment import *
+from .unsaturated_flow import *
 from .swrc import *
